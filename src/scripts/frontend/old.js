ea2cffc23f0bc4968acb63a8df09d619d9d801bb
Nadav Tasger <tashernadav@gmail.com>
2019-12-20 00:38:17 +0200

    Hint

--- src/scripts/frontend/old.js
+++ src/scripts/frontend/old.js
@@ -1,4 +1,4 @@
-const SERVER_SIGNATURE = "h1ins9nfvvf5z0ny6167mgivtyo1i9pbnydj0my3emt9tcok57znn183gn68zqv5";
+const SERVER_SIGNATURE = "----------------------------------------------------------------";
 
 const TYPES = [
     "Biscotti",
